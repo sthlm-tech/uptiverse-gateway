2dcacc30d8bd15c7397d7854fc457f1997dea260
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-07-21 16:11:26 +0200

    updated ms-core version

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ App.Express.use(cookieParser());
 
 App.Express.use("/api/*", function (req, res) {
 	var startIndex = 2;
+
 	var urlParts = req.originalUrl.split("/");
 	var application = urlParts[startIndex];
 	var applicationRoute = "https://uptiverse-" + application + ".herokuapp.com";
 	var route = urlParts.slice(startIndex, urlParts.length).reduce(function(url, part) { return url + "/" + part; }, "");
 
 	var finalRoute = applicationRoute + route;
-
 	var token = req.cookies.id_token;
-
 	request(req.method,finalRoute)
  		.send(req.body)
 		.set('Authorization', "JWT " + token)
  		.end(function(err, response){
 	     if (err || !response.ok) {
 	       res.send(response.text);
-	     } else {
+	     }
+       else {
 	       res.send(response.body);
 	     }
 	   });
